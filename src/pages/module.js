13575cac50c38e3aab20b7c1c1cd5772cda79c01
Christian Burkhart <burkhart.christian@gmail.com>
2019-03-21 23:51:17 +0100

    feat: update styles

--- src/pages/module.js
+++ src/pages/module.js
@@ -15,7 +15,7 @@ import Audio from "../components/audio";
 import SingleChoice from "../components/questions/singlechoice";
 import MultipleChoice from "../components/questions/multiplechoice";
 import OrderQuestion from "../components/questions/orderquestion";
-// import Flipcard from "../components/questions/flipcard";
+import VideoModeling from "../components/questions/youtubevideomodeling";
 
 // Icons 
 import { FaCaretLeft, FaChevronLeft, FaChevronRight, FaFolderPlus, FaFolderMinus, FaAngleRight, FaAngleLeft } from "react-icons/fa";
@@ -390,8 +390,8 @@ const renderAst = new rehypeReact({
     "audio": Audio,
     "singlechoice": SingleChoice,
     "multiplechoice": MultipleChoice,
-    "orderquestion": OrderQuestion
-    // "flipcard": Flipcard
+    "orderquestion": OrderQuestion,
+    "videomodeling": VideoModeling
   },
 }).Compiler
 
